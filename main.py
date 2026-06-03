4fffbf08157ec96de1a8ab930b3a29f91689d62b
Blaarg <157559082+Blaarg987@users.noreply.github.com>
2026-06-03 00:19:49 +0000

    First commit on Chapter 2 Lesson 2

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print("Starting Asteroids")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+
+
+if __name__ == "__main__":
+    main()
