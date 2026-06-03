4ddb88c2660df882b8c1c9452d6f1f36ff49c36e
Austin Powell <powellaustin944@gmail.com>
2026-06-02 21:34:11 -0400

    Finished Chapter 3 lesson 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -8,13 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     print(f"Screen width: {SCREEN_WIDTH}")
     pygame.init()
+    dt = 0.0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
