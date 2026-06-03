e416f6d51772ec6926331926b946f61ba7cb5ad6
Blaarg <157559082+Blaarg987@users.noreply.github.com>
2026-06-03 00:34:49 +0000

    Added code to draw the screen

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
 
 def main():
     print("Starting Asteroids")
     print(f"Screen height: {SCREEN_HEIGHT}")
     print(f"Screen width: {SCREEN_WIDTH}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
